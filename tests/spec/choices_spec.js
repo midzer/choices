d0b78438bb91768649a12361f8f4e4585cd73aac
Josh Johnson <josh@joshuajohnson.co.uk>
2017-01-01 15:59:43 +0000

    Expose fuse options in config

--- tests/spec/choices_spec.js
+++ tests/spec/choices_spec.js
@@ -2,6 +2,30 @@ import 'whatwg-fetch';
 import 'es6-promise';
 import Choices from '../../assets/scripts/src/choices.js';
 
+if (typeof Object.assign != 'function') {
+  Object.assign = function (target, varArgs) { // .length of function is 2
+    if (target == null) { // TypeError if undefined or null
+      throw new TypeError('Cannot convert undefined or null to object');
+    }
+
+    var to = Object(target);
+
+    for (var index = 1; index < arguments.length; index++) {
+      var nextSource = arguments[index];
+
+      if (nextSource != null) { // Skip over if undefined or null
+        for (var nextKey in nextSource) {
+          // Avoid bugs when hasOwnProperty is shadowed
+          if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+            to[nextKey] = nextSource[nextKey];
+          }
+        }
+      }
+    }
+    return to;
+  };
+}
+
 describe('Choices', () => {
 
   afterEach(function() {
