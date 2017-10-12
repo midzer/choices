d5167b0106fdeb064814a8e014e70b3953a20812
Josh Johnson <josh@joshuajohnson.co.uk>
2017-10-12 12:43:53 +0100

    Add further container tests

--- src/scripts/src/components/container.test.js
+++ src/scripts/src/components/container.test.js
@@ -31,7 +31,6 @@ describe('Container', () => {
     expect(instance.classNames).to.eql(DEFAULT_CLASSNAMES);
   });
 
-
   describe('addEventListeners', () => {
     let addEventListenerStub;
 
@@ -86,6 +85,30 @@ describe('Container', () => {
     });
   });
 
+  // describe('shouldFlip', () => { });
+
+  describe('setActiveDescendant', () => {
+    it('sets element\'s aria-activedescendant attribute with passed descendant ID', () => {
+      const activeDescendantID = '1234';
+      expect(instance.element.getAttribute('aria-activedescendant')).to.equal(null);
+      instance.setActiveDescendant(activeDescendantID);
+      expect(instance.element.getAttribute('aria-activedescendant')).to.equal(activeDescendantID);
+    });
+  });
+
+  describe('removeActiveDescendant', () => {
+    it('remove elememnt\'s aria-activedescendant attribute', () => {
+      const activeDescendantID = '1234';
+      instance.element.setAttribute('aria-activedescendant', activeDescendantID);
+      expect(instance.element.getAttribute('aria-activedescendant')).to.equal(activeDescendantID);
+      instance.removeActiveDescendant();
+      expect(instance.element.getAttribute('aria-activedescendant')).to.equal(null);
+    });
+  });
+
+  // describe('open', () => { });
+  // describe('close', () => { });
+
   describe('focus', () => {
     let focusStub;
 
@@ -104,22 +127,56 @@ describe('Container', () => {
     });
   });
 
-  describe('setActiveDescendant', () => {
-    it('sets element\'s aria-activedescendant attribute with passed descendant ID', () => {
-      const activeDescendantID = '1234';
-      expect(instance.element.getAttribute('aria-activedescendant')).to.equal(null);
-      instance.setActiveDescendant(activeDescendantID);
-      expect(instance.element.getAttribute('aria-activedescendant')).to.equal(activeDescendantID);
+  // describe('addFocusState', () => { });
+  // describe('removeFocusState', () => { });
+  // describe('enable', () => {});
+  // describe('disable', () => {});
+
+  describe('addLoadingState', () => {
+    beforeEach(() => {
+      instance.removeLoadingState();
+    });
+
+    it('adds loading state class', () => {
+      expect(instance.element.classList.contains(DEFAULT_CLASSNAMES.loadingState)).to.equal(false);
+      instance.addLoadingState();
+      expect(instance.element.classList.contains(DEFAULT_CLASSNAMES.loadingState)).to.equal(true);
+    });
+
+    it('sets aria-busy attribute to true', () => {
+      expect(instance.element.getAttribute('aria-busy')).to.equal(null);
+      instance.addLoadingState();
+      expect(instance.element.getAttribute('aria-busy')).to.equal('true');
+    });
+
+    it('sets isLoading flag to false', () => {
+      expect(instance.isLoading).to.equal(false);
+      instance.addLoadingState();
+      expect(instance.isLoading).to.equal(true);
     });
   });
 
-  describe('removeActiveDescendant', () => {
-    it('remove elememnt\'s aria-activedescendant attribute', () => {
-      const activeDescendantID = '1234';
-      instance.element.setAttribute('aria-activedescendant', activeDescendantID);
-      expect(instance.element.getAttribute('aria-activedescendant')).to.equal(activeDescendantID);
-      instance.removeActiveDescendant();
-      expect(instance.element.getAttribute('aria-activedescendant')).to.equal(null);
+  describe('removeLoadingState', () => {
+    beforeEach(() => {
+      instance.addLoadingState();
+    });
+
+    it('removes loading state class', () => {
+      expect(instance.element.classList.contains(DEFAULT_CLASSNAMES.loadingState)).to.equal(true);
+      instance.removeLoadingState();
+      expect(instance.element.classList.contains(DEFAULT_CLASSNAMES.loadingState)).to.equal(false);
+    });
+
+    it('removes aria-busy attribute', () => {
+      expect(instance.element.getAttribute('aria-busy')).to.equal('true');
+      instance.removeLoadingState();
+      expect(instance.element.getAttribute('aria-busy')).to.equal(null);
+    });
+
+    it('sets isLoading flag to true', () => {
+      expect(instance.isLoading).to.equal(true);
+      instance.removeLoadingState();
+      expect(instance.isLoading).to.equal(false);
     });
   });
 });
