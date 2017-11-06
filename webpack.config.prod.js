cc11d6bfd6d7d25eadce1bce54f16630b0f7d494
Raymond Zhou <option17@gmail.com>
2017-11-06 17:06:38 -0500

    Update to Webpack 3

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,54 +3,61 @@ const pkg = require('./package.json');
 const webpack = require('webpack');
 const wrapperPlugin = require('wrapper-webpack-plugin');
 const banner = `/*! ${ pkg.name } v${ pkg.version } | (c) ${ new Date().getFullYear() } ${ pkg.author } | ${ pkg.homepage } */ \n`;
-const minimize = process.argv.includes('--minimize');
 
-const config = {
-  devtool: minimize ? false : 'cheap-module-source-map',
-  entry: [
-    './src/scripts/src/choices'
-  ],
-  output: {
-    path: path.join(__dirname, '/src/scripts/dist'),
-    filename: minimize ? 'choices.min.js' : 'choices.js',
-    publicPath: '/src/scripts/dist/',
-    library: 'Choices',
-    libraryTarget: 'umd',
-  },
-  libraryTarget: 'umd',
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        // This has effect on the react lib size
-        'NODE_ENV': JSON.stringify('production'),
-      }
-    }),
-    new wrapperPlugin({
-      header: banner,
-    }),
-  ],
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: /(node_modules|bower_components)/,
-      loaders: ['babel'],
-      include: path.join(__dirname, 'src/scripts/src')
-    }]
-  }
-};
+module.exports = (env) => {
+  const minimize = !!(env && env.minimize);
 
-if (minimize) {
-  config.plugins.unshift(new webpack.optimize.UglifyJsPlugin({
-    sourceMap: false,
-    mangle: true,
+  const config = {
+    devtool: minimize ? false : 'cheap-module-source-map',
+    entry: [
+      './src/scripts/src/choices'
+    ],
     output: {
-      comments: false
+      path: path.join(__dirname, '/src/scripts/dist'),
+      filename: minimize ? 'choices.min.js' : 'choices.js',
+      publicPath: '/src/scripts/dist/',
+      library: 'Choices',
+      libraryTarget: 'umd',
     },
-    compress: {
-      warnings: false,
-      screw_ie8: true
-    }
-  }));
-}
+    plugins: [
+      new webpack.optimize.ModuleConcatenationPlugin(),
+      new webpack.DefinePlugin({
+        'process.env': {
+          // This has effect on the react lib size
+          'NODE_ENV': JSON.stringify('production'),
+        }
+      }),
+      new wrapperPlugin({
+        header: banner,
+      }),
+    ],
+    module: {
+      rules: [{
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        include: path.join(__dirname, 'src/scripts/src'),
+        options: {
+          babelrc: false,
+          presets: [['es2015', { modules: false }], 'stage-2'],
+        },
+      }],
+    },
+  };
 
-module.exports = config;
+  if (minimize) {
+    config.plugins.unshift(new webpack.optimize.UglifyJsPlugin({
+      sourceMap: false,
+      mangle: true,
+      output: {
+        comments: false
+      },
+      compress: {
+        warnings: false,
+        screw_ie8: true
+      }
+    }));
+  }
+
+  return config;
+};
