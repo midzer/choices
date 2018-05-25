25309c91fcf228206e2c3139c5e7301ed02f15f9
Josh Johnson <josh@joshuajohnson.co.uk>
2018-05-25 08:53:50 +0100

    Use dep for event polyfill

--- src/scripts/src/lib/polyfills.js
+++ src/scripts/src/lib/polyfills.js
@@ -1,18 +1,2 @@
 import 'core-js/fn/array/from';
-import 'core-js/fn/array/find';
-
-(function CustomEventPolyfill() {
-  if (typeof window.CustomEvent === 'function') {
-    return false;
-  }
-
-  function CustomEvent (event, params = { bubbles: false, cancelable: false, detail: undefined }) {
-    const evt = document.createEvent('CustomEvent');
-    evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
-    return evt;
-  }
-
-  CustomEvent.prototype = window.Event.prototype;
-
-  window.CustomEvent = CustomEvent;
-}());
+import 'custom-event-polyfill';
