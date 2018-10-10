7d3c8daf07226a153c7a81d075b8cb5354ff5e17
Josh Johnson <josh@joshuajohnson.co.uk>
2018-10-10 12:36:43 +0100

    Install cypress

---
+++ cypress/integration/choices_spec.js
@@ -0,0 +1,12 @@
+describe('Choices', () => {
+  describe('text element', () => {
+    it('works', () => {
+      cy.visit('/');
+      expect(true).to.equal(true);
+    });
+  });
+
+  describe('select-one element', () => {});
+
+  describe('select-multiple element', () => {});
+});
