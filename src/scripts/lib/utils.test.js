55b356ec69d8e1765b36bedb077f905f0d50a0a1
Josh Johnson <josh@joshuajohnson.co.uk>
2019-02-12 18:04:50 +0000

    Add travis pipeline (#516)
    * Update travis.yml
    
    * Simplify ci pipeline
    
    * Include css in bundle size check
    
    * Set npm username via env var
    
    * Specify branches
    
    * Add build scripts
    
    * Consistent ifs
    
    * Add linting stage to pipeline
    
    * Cache npm
    
    * Cache cypress & npm folders + install using ci method
    
    * Unignore package-lock

--- src/scripts/lib/utils.test.js
+++ src/scripts/lib/utils.test.js
@@ -251,6 +251,7 @@ describe('utils', () => {
   describe('regexFilter', () => {
     it('tests given regex against given value', () => {
       // An email address regex
+      // eslint-disable-next-line
       const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
       expect(regexFilter('joe@bloggs.com', regex)).to.equal(true);
