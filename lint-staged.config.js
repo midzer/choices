472e0dff39e251741453f7f04d3c2efe793a32c6
Konstantin Vyatkin <tino@vtkn.io>
2019-10-24 13:13:06 -0400

    [Tooling] use lint-stagged and unleash full Prettier (#692)
    * switch to lint-stagged
    
    * separate prettier config into external file
    
    * recommend and configure VSCode extensions

---
+++ lint-staged.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  '*.js': ['eslint --fix --quiet -f visualstudio', 'git add'],
+  '*.{ts,scss,yaml,yml,md,html,json,babelrc,eslintrc}': [
+    'prettier --write',
+    'git add',
+  ],
+  'src/icons/*.svg': [
+    'prettier --write --parser=html --html-whitespace-sensitivity=ignore',
+    'git add',
+  ],
+  '.codecov.yml': () =>
+    'curl -f --silent --data-binary @.codecov.yml https://codecov.io/validate',
+  'src/scripts/**/*.js': () => 'mocha',
+};
