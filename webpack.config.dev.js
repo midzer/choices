92d5e49fb6f73e98bb78d01a659d3a2b5f08f6c7
Konstantin Vyatkin <tino@vtkn.io>
2019-10-28 15:43:21 -0400

    [Tooling] VSCode Debug configurations and project settings (#705)
    * configure tasks
    
    * configure VSCode debugging
    
    * adjust VSCode project settings
    
    * fix bdd
    
    * fix cypress path on macos/linux
    
    * enforce checkJs
    
    * make buildAndWatch default build task
    
    * recommend and configure mocha-sidebar
    
    * tune settings

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -5,16 +5,17 @@ const baseConfig = require('./webpack.config.base');
 
 module.exports = deepMerge(
   baseConfig,
-  {
+  /** @type {import('webpack').Configuration} */ ({
     mode: 'development',
     output: {
       path: path.resolve(__dirname, './public'),
       filename: 'choices.min.js',
       publicPath: 'http://localhost:3001/assets/scripts/',
     },
+    devtool: 'source-map',
     entry: ['webpack/hot/dev-server', 'webpack-hot-middleware/client'],
     plugins: [new HotModuleReplacementPlugin()],
-  },
+  }),
   {
     arrayMerge(target, source) {
       return [...source, ...target];
