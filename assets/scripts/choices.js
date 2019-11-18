523ed01ffc80bf1c2f776abd59ae29c93ad835d8
GitHub Action <action@github.com>
2019-11-18 09:55:07 +0000

    Update build files 🏗

--- assets/scripts/choices.js
+++ assets/scripts/choices.js
@@ -1,4 +1,4 @@
-/*! choices.js v9.0.0 | © 2019 Josh Johnson | https://github.com/jshjohnson/Choices#readme */
+/*! choices.js v9.0.1 | © 2019 Josh Johnson | https://github.com/jshjohnson/Choices#readme */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
