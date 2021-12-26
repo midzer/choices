a8862e78becf43d0d58c5bc05051a14bf1dbfcbc
Matt Triff <matt.triff@gmail.com>
2021-12-26 16:38:09 -0500

    Add typings and declaration maps

---
+++ public/types/src/index.d.ts
@@ -0,0 +1,6 @@
+import Choices from './scripts/choices';
+export * from './scripts/constants';
+export * from './scripts/defaults';
+export { default as templates } from './scripts/templates';
+export default Choices;
+//# sourceMappingURL=index.d.ts.map
---
+++ public/types/src/scripts/interfaces/passed-element-type.d.ts
@@ -0,0 +1,2 @@
+export declare type PassedElementType = 'text' | 'select-one' | 'select-multiple';
+//# sourceMappingURL=passed-element-type.d.ts.map
